34e5f20ec1c4cd623a4400f3efad60b7f4214a0a
Toumi <Toumibusiness0@gmail.com>
2023-11-29 23:18:55 +0100

    initial git - server

---
+++ server/index.js
@@ -0,0 +1,185 @@
+const PORT = 8000
+
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const { v4: uuidv4 } = require('uuid')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+const bodyParser = require('body-parser')
+
+const uri = 'mongodb+srv://toumibusiness0:mypassword@cluster0.twesmhn.mongodb.net/?retryWrites=true&w=majority'
+
+
+ 
+const app = express()
+ 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.get(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.json('hello to my app')
+})
+
+app.post('/signup', async (req, res) => {
+    const Client = new MongoClient(uri)
+    console.log("request.body", req.body)
+    const { email, password } = req.body
+
+    const generateduserID = uuidv4()
+    const hashed_password = await bcrypt.hash(password, 10) 
+
+    try {
+        await Client.connect()
+        const database = Client.db('app-data')
+        const users = database.collection('users')
+
+        const existingUser = await users.findOne({ email })
+
+        if (existingUser) {
+            return res.status(409).send('User already exists. Please login')
+        }
+
+        const sanitizedEmail = email.toLowerCase()
+
+        const data = {
+            user_id: generateduserID,
+            email: sanitizedEmail,
+            hashed_password: hashed_password
+        }
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn: 68 * 24,
+        })
+
+        res.status(201).json({ token, user_id: generateduserID })
+    } catch (err) {
+        console.log(err)
+    }
+
+
+})
+
+app.post('/login', async (req, res) => {
+    const Client = new MongoClient(uri)
+    const { email, password} = req.body
+
+    try {
+        await Client.connect()
+        const database = Client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({ email })
+
+        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+        if (user && correctPassword) {
+            const token = jwt.sign(user, email, {
+                expiresIn: 60 * 24
+            })
+            res.status(201).json({ token, UserID: user.user_id })
+        }
+        res.status(400).send('Invalid Credentials')
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+
+
+
+
+
+app.put('/user', async ( req, res) => {
+    const Client = new MongoClient(uri)
+    const formData = req.body.formData
+    try {
+        await Client.connect()
+        const database = Client.db('app-data')
+        const users = database.collection('users')
+
+        const query = { user_id: formData.user_id}
+        const updateDocument = {
+            $set: {
+                name: formData.name,
+                age: formData.age,
+                show_sex: formData.show_sex,
+                sex: formData.sex,
+                sex_interest: formData.sex_interest,
+                url: formData.url,
+                likes: formData.likes,
+                matches: formData.matches
+            }
+        }
+        const insertedUser = await users.updateOne(query, updateDocument)
+        res.send(insertedUser)
+    } finally {
+        await Client.close()
+    }
+})
+
+
+app.get('/user', async(req, res) => {
+    const Client = new MongoClient(uri)
+    const user_id = req.query.user_id
+
+
+    try {
+        await Client.connect()
+        const database = Client.db('app-data')
+        const users = database.collection('users')
+        const query = { user_id: user_id }
+
+        const user = await users.findOne(query)
+        res.send(user)
+    } finally {
+        await Client.close()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/users', async(req, res) => {
+    const Client = new MongoClient(uri)
+
+    try{
+        await Client.connect()
+        const database = Client.db('app-data')
+        const users = database.collection('users')
+
+        const returnedusers = await users.find().toArray()
+        res.send(returnedusers)
+    } finally {
+        await Client.close()
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log('Server running on PORT '+ PORT))
