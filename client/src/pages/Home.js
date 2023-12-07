947600f439f8063f8851a844902bac8759fd0b12
MAIMOUN Bakero <bakero.maimoun@gmail.com>
2023-12-07 17:45:57 +0100

    fix display card images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,13 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import Messaging from './Messaging'
 
 const messages = [
-  { user: 'Glen', lastMessage: 'hellooo', img: 'https://drive.google.com/file/d/1wngUDrWezjjR6zCcQEPewr8crkwlbgSQ/view?usp=drive_link' },
-  { user: 'Jake', lastMessage: 'cute kitty', img: 'https://drive.google.com/file/d/17cKerHHPv88cw8LFi3GdJu74ANeF77jI/view?usp=drive_link' }
+  { user: 'Glen', lastMessage: 'hellooo', img: 'https://images-ext-2.discordapp.net/external/qaBxpYqIBpJnCWcQ_QemnowDABhtW5XkFEZuNvR6h00/https/lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaDOEkK_sph-JQQBD-4FpKrIUGHo9MOVuuWUwffZKWNojB1mKsyVR0gTQky2nwv2TU74IwyDKvzo8ddSUSBEhyAlaWsy%3Dw1920-h955?format=webp' },
+  { user: 'Jake', lastMessage: 'cute kitty', img: 'https://images-ext-2.discordapp.net/external/xhF6n69zTSYIMsSpijOhQkGPDcxlyksoXIjt915xxHU/https/lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaB4s5J1ujU-ePuhfRtWKWf-NNlyHqlamzsNizjBzhw0x9awEU08MZD8T2TBtD3aFqFGSyOhcq8_lP52WUCuHLidZGgFRQ%3Dw1920-h955?format=webp' }
 ]
 
 const matches = [
-  { user: 'Glen', img: 'https://drive.google.com/file/d/1wngUDrWezjjR6zCcQEPewr8crkwlbgSQ/view?usp=drive_link' },
-  { user: 'Jake', img: 'https://drive.google.com/file/d/17cKerHHPv88cw8LFi3GdJu74ANeF77jI/view?usp=drive_link' }
+  { user: 'Glen', img: 'https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaB4s5J1ujU-ePuhfRtWKWf-NNlyHqlamzsNizjBzhw0x9awEU08MZD8T2TBtD3aFqFGSyOhcq8_lP52WUCuHLidZGgFRQ=w1249-h955?format=webp' },
+  { user: 'Jake', img: 'https://images-ext-2.discordapp.net/external/qaBxpYqIBpJnCWcQ_QemnowDABhtW5XkFEZuNvR6h00/https/lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaDOEkK_sph-JQQBD-4FpKrIUGHo9MOVuuWUwffZKWNojB1mKsyVR0gTQky2nwv2TU74IwyDKvzo8ddSUSBEhyAlaWsy%3Dw1920-h955?format=webp' }
 ]
 
 const Home = () => {
