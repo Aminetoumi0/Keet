72e5595d782e0284131b7881576aeafdf4342bba
Aminetoumi0 <125613112+Aminetoumi0@users.noreply.github.com>
2023-12-11 13:48:19 +0100

    Update LandingPage.js

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,13 +2,17 @@ import Nav from '../components/design/Nav'
 import AuthModal from '../components/AuthModal'
 import { useState } from 'react'
 
+// Functional component 'LandingPage'
 const LandingPage = () => {
+  // State for managing modal visibility and sign-up mode
   const [showModal, setshowModal] = useState(false)
   const [IsSignUp, setIsSignUp] = useState(true)
+  // Dummy authentication token
   const authToken = false
-
+  // Handler for button click
   const handleClick = () => {
     console.log('clicked')
+    // Show modal and set it to sign-up mode
     setshowModal(true)
     setIsSignUp(true)
   }
