a329d912d377f9950e2d50f675eca034d178fbe0
Aminetoumi0 <125613112+Aminetoumi0@users.noreply.github.com>
2023-12-11 13:41:06 +0100

    Update Messages.js

--- client/src/components/business/Messages.js
+++ client/src/components/business/Messages.js
@@ -1,23 +1,25 @@
+// Importing necessary components and styles from Material-UI
 import { makeStyles } from '@mui/styles'
 
 import Card from '../design/Card/Card'
 
+// Define styles using makeStyles
 
 const useStyles = makeStyles({
   content: { display: 'flex', alignItems: 'flex-start', flexDirection: 'column' },
 
 
 })
-
+// Functional component 'Messages' that receives props
 const Messages = ({ messages, onMessageClick }) => {
-
+  // Using the defined styles
   const classes = useStyles()
 
-
+  // Handler for clicking on a message
   const handleClick = (msg) => () => {
     onMessageClick(msg)
   }
-
+  // Rendering the list of messages using the Card component
   return messages.map((msg) => (
     <Card
       key={msg.user}
